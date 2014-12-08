a261ef71e0305eaa8a5b5015d97220e1d322bb5f
atombrot <atombrot@gonzales>
2014-12-08 18:35:48 +0100

    Removed simple tile grid serializer and tests (currently probably even out of scope of the project. It uses a file writer and I don't think the hooks for serialization should go that deep). Also including sources now in the package

--- src/main/java/ch/digitalmeat/grid/util/SimpleTileGridSerializer.java
+++ src/main/java/ch/digitalmeat/grid/util/SimpleTileGridSerializer.java
@@ -1,94 +1,94 @@
-package ch.digitalmeat.grid.util;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Map;
-
-import ch.digitalmeat.grid.ChunkGrid;
-import ch.digitalmeat.grid.chunk.ChunkBase;
-import ch.digitalmeat.grid.tile.ChunkTileBase;
-import ch.digitalmeat.util.Point;
-
-public class SimpleTileGridSerializer<T extends ChunkTileBase<T, C>, C extends ChunkBase<T, C>> implements TileGridSerializer<T, C> {
-
-	@Override
-	public ChunkGrid<T, C> readGrid(String gridFile) {
-		return null;
-	}
-
-	@Override
-	public void writeGrid(String path, ChunkGrid<T, C> grid) {
-		try {
-			File file = new File(path + ".grid");
-			File parent = file.getParentFile();
-			if (!parent.exists()) {
-				parent.mkdirs();
-			}
-			FileWriter fileWriter = new FileWriter(file);
-			BufferedWriter writer = new BufferedWriter(fileWriter);
-			writeGrid(grid, writer, path);
-			writer.close();
-			fileWriter.close();
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
-	}
-
-	private void writeGrid(ChunkGrid<T, C> grid, BufferedWriter writer, String path) throws IOException {
-		writer.write("chunksize:" + grid.chunkWidth + ";" + grid.chunkHeight);
-		writer.write("\n");
-		Map<Point, C> chunks = grid.chunks;
-		writer.write("chunks:" + grid.chunkWidth + ";" + grid.chunkHeight);
-		writer.write("\n");
-
-		for (Point coordinates : chunks.keySet()) {
-
-			C chunk = chunks.get(coordinates);
-			String name = coordinates.x + "." + coordinates.y;
-			writer.write(name);
-			writer.write("\n");
-
-			writeChunk(path + "." + name, chunk);
-		}
-	}
-
-	@Override
-	public C readChunk(String chunkFile) {
-		return null;
-	}
-
-	@Override
-	public void writeChunk(String path, C chunk) {
-		try {
-			File file = new File(path);
-			File parent = file.getParentFile();
-			if (!parent.exists()) {
-				parent.mkdirs();
-			}
-			FileWriter fileWriter = new FileWriter(file);
-			BufferedWriter writer = new BufferedWriter(fileWriter);
-			writeChunk(chunk, writer, path);
-			writer.close();
-			fileWriter.close();
-
-		} catch (Exception ex) {
-			throw new RuntimeException();
-		}
-	}
-
-	private void writeChunk(C chunk, BufferedWriter writer, String path) throws IOException {
-		for (int y = chunk.grid.chunkHeight - 1; y >= 0; y--) {
-			for (int x = 0; x < chunk.grid.chunkWidth; x++) {
-				T tile = chunk.get(x, y);
-				if (tile == null) {
-					writer.write("!");
-				} else {
-					writer.write("T");
-				}
-			}
-			writer.write("\n");
-		}
-	}
-}
+//package ch.digitalmeat.grid.util;
+//
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.Map;
+//
+//import ch.digitalmeat.grid.ChunkGrid;
+//import ch.digitalmeat.grid.chunk.ChunkBase;
+//import ch.digitalmeat.grid.tile.ChunkTileBase;
+//import ch.digitalmeat.util.Point;
+//
+//public class SimpleTileGridSerializer<T extends ChunkTileBase<T, C>, C extends ChunkBase<T, C>> implements TileGridSerializer<T, C> {
+//
+//	@Override
+//	public ChunkGrid<T, C> readGrid(String gridFile) {
+//		return null;
+//	}
+//
+//	@Override
+//	public void writeGrid(String path, ChunkGrid<T, C> grid) {
+//		try {
+//			File file = new File(path + ".grid");
+//			File parent = file.getParentFile();
+//			if (!parent.exists()) {
+//				parent.mkdirs();
+//			}
+//			FileWriter fileWriter = new FileWriter(file);
+//			BufferedWriter writer = new BufferedWriter(fileWriter);
+//			writeGrid(grid, writer, path);
+//			writer.close();
+//			fileWriter.close();
+//		} catch (Exception ex) {
+//			throw new RuntimeException(ex);
+//		}
+//	}
+//
+//	private void writeGrid(ChunkGrid<T, C> grid, BufferedWriter writer, String path) throws IOException {
+//		writer.write("chunksize:" + grid.chunkWidth + ";" + grid.chunkHeight);
+//		writer.write("\n");
+//		Map<Point, C> chunks = grid.chunks;
+//		writer.write("chunks:" + grid.chunkWidth + ";" + grid.chunkHeight);
+//		writer.write("\n");
+//
+//		for (Point coordinates : chunks.keySet()) {
+//
+//			C chunk = chunks.get(coordinates);
+//			String name = coordinates.x + "." + coordinates.y;
+//			writer.write(name);
+//			writer.write("\n");
+//
+//			writeChunk(path + "." + name, chunk);
+//		}
+//	}
+//
+//	@Override
+//	public C readChunk(String chunkFile) {
+//		return null;
+//	}
+//
+//	@Override
+//	public void writeChunk(String path, C chunk) {
+//		try {
+//			File file = new File(path);
+//			File parent = file.getParentFile();
+//			if (!parent.exists()) {
+//				parent.mkdirs();
+//			}
+//			FileWriter fileWriter = new FileWriter(file);
+//			BufferedWriter writer = new BufferedWriter(fileWriter);
+//			writeChunk(chunk, writer, path);
+//			writer.close();
+//			fileWriter.close();
+//
+//		} catch (Exception ex) {
+//			throw new RuntimeException();
+//		}
+//	}
+//
+//	private void writeChunk(C chunk, BufferedWriter writer, String path) throws IOException {
+//		for (int y = chunk.grid.chunkHeight - 1; y >= 0; y--) {
+//			for (int x = 0; x < chunk.grid.chunkWidth; x++) {
+//				T tile = chunk.get(x, y);
+//				if (tile == null) {
+//					writer.write("!");
+//				} else {
+//					writer.write("T");
+//				}
+//			}
+//			writer.write("\n");
+//		}
+//	}
+// }
--- src/test/java/ch/digitalmeat/grid/SimpleTileGridSerializerTest.java
+++ src/test/java/ch/digitalmeat/grid/SimpleTileGridSerializerTest.java
@@ -1,19 +1,19 @@
-package ch.digitalmeat.grid;
-
-import org.junit.Test;
-
-import ch.digitalmeat.grid.util.SimpleTileGridSerializer;
-
-public class SimpleTileGridSerializerTest {
-
-	private static final String TESTFOLDER = "testdata/";
-
-	@Test
-	public void testOutput() {
-		SimpleTileGridSerializer<TestTile, TestChunk> serializer = new SimpleTileGridSerializer<TestTile, TestChunk>();
-		ChunkGrid<TestTile, TestChunk> grid = new ChunkGrid<TestTile, TestChunk>(new TestFactory(), 32, 32);
-		grid.ensureTile(1, 1);
-		serializer.writeGrid(TESTFOLDER + "testOutput", grid);
-	}
-
-}
+//package ch.digitalmeat.grid;
+//
+//import org.junit.Test;
+//
+//import ch.digitalmeat.grid.util.SimpleTileGridSerializer;
+//
+//public class SimpleTileGridSerializerTest {
+//
+//	private static final String TESTFOLDER = "testdata/";
+//
+//	@Test
+//	public void testOutput() {
+//		SimpleTileGridSerializer<TestTile, TestChunk> serializer = new SimpleTileGridSerializer<TestTile, TestChunk>();
+//		ChunkGrid<TestTile, TestChunk> grid = new ChunkGrid<TestTile, TestChunk>(new TestFactory(), 32, 32);
+//		grid.ensureTile(1, 1);
+//		serializer.writeGrid(TESTFOLDER + "testOutput", grid);
+//	}
+//
+// }
